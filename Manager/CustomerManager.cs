b2c65a7b0ba86a813ebdad4958bdf4c6947987da
THAO VY <lengocthaovy1587@gmail.com>
2025-03-18 21:50:39 +0700

    customer class, customermanager

---
+++ Manager/CustomerManager.cs
@@ -0,0 +1,128 @@
+﻿using MarketManagement.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using MarketManagement.UseControl;
+
+namespace MarketManagement.Manager
+{
+    public class CustomerManager : IManager<Customer>
+    {
+        private List<Customer> customers;
+        private readonly string filePath;
+
+        public CustomerManager()
+        {
+            filePath = "customers.json";
+            customers = LoadFromFile();
+        }
+
+        private List<Customer> LoadFromFile()
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string jsonData = File.ReadAllText(filePath);
+                    return JsonConvert.DeserializeObject<List<Customer>>(jsonData) ?? new List<Customer>();
+                }
+            }
+            catch { }
+            return new List<Customer>();
+        }
+
+        private void SaveToFile()
+        {
+            try
+            {
+                string jsonData = JsonConvert.SerializeObject(customers, Formatting.Indented);
+                File.WriteAllText(filePath, jsonData);
+            }
+            catch { }
+        }
+
+        public bool Add(Customer customer)
+        {
+            if (customers.Any(c => c.Id == customer.Id))
+            {
+                throw new Exception("Customer ID already exists!");
+            }
+            customers.Add(customer);
+            SaveToFile();
+            return true;
+        }
+
+        public bool Update(Customer customer)
+        {
+            int index = customers.FindIndex(c => c.Id == customer.Id);
+            if (index != -1)
+            {
+                customers[index] = customer;
+                SaveToFile();
+                return true;
+            }
+            return false;
+        }
+
+        public bool Remove(string customerId)
+        {
+            customers.RemoveAll(c => c.Id == customerId);
+            SaveToFile();
+            return true;
+        }
+
+        public Customer GetById(string customerId)
+        {
+            return customers.FirstOrDefault(c => c.Id == customerId);
+        }
+
+        public List<Customer> GetAll()
+        {
+            return customers;
+        }
+
+        // Các phương thức bổ sung nếu cần
+        public List<Customer> GetVIPCustomers()
+        {
+            List<Customer> vipCustomers = new List<Customer>();
+            foreach (Customer customer in customers)
+            {
+                if (customer.IsVIP)
+                {
+                    vipCustomers.Add(customer);
+                }
+            }
+            return vipCustomers;
+        }
+
+        public Customer GetByPhoneNumber(string phoneNumber)
+        {
+            foreach (Customer customer in customers)
+            {
+                if (customer.PhoneNumber == phoneNumber)
+                {
+                    return customer;
+                }
+            }
+            return null;
+        }
+
+        public List<Customer> SearchByName(string searchTerm)
+        {
+            List<Customer> results = new List<Customer>();
+            foreach (Customer customer in customers)
+            {
+                if (customer.CustomerName != null &&
+                    customer.CustomerName.IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    results.Add(customer);
+                }
+            }
+            return results;
+        }
+    }
+}
---
+++ Model/Customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketManagement.Model
+{
+    public class Customer : BaseEntity, ISerializable
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public bool IsVIP { get; set; }
+
+        public Customer(string name, string address, string phoneNumber, string email, bool isVIP = false)
+        {
+            CustomerName = name;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            IsVIP = isVIP;
+        }
+
+        protected Customer(SerializationInfo info, StreamingContext context)
+        {
+            Id = info.GetString("Id");
+            CustomerName = info.GetString("Name");
+            Address = info.GetString("Address");
+            PhoneNumber = info.GetString("PhoneNumber");
+            Email = info.GetString("Email");
+            IsVIP = info.GetBoolean("IsVIP");
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Id", Id);
+            info.AddValue("Name", CustomerName);
+            info.AddValue("Address", Address);
+            info.AddValue("PhoneNumber", PhoneNumber);
+            info.AddValue("Email", Email);
+            info.AddValue("IsVIP", IsVIP);
+        }
+
+        public override string GenerateId()
+        {
+            if (IsVIP)
+            {
+                return "VIP" + DateTime.Now.ToString("yyyyMMddHHmmss");
+            }
+            else
+            {
+                return "CUS" + DateTime.Now.ToString("yyyyMMddHHmmss");
+            }
+        }
+
+        public override bool Validate()
+        {
+           
+            return !string.IsNullOrEmpty(CustomerName) && !string.IsNullOrEmpty(PhoneNumber);
+        }
+    }
+}
