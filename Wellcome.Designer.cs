c799c21ec1d0c22226e19b901459b8d79239eb33
Admin <Admin@ADMIN-PC>
2025-03-28 14:42:18 +0700

    order

--- Wellcome.Designer.cs
+++ Wellcome.Designer.cs
@@ -74,7 +74,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1061, 590);
+            this.ClientSize = new System.Drawing.Size(1061, 536);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
