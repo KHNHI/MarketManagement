2df596b1695ceff8eea8f3589002e33280eeb4c2
Admin <Admin@ADMIN-PC>
2025-03-29 22:43:09 +0700

    change wellcome

--- Wellcome.Designer.cs
+++ Wellcome.Designer.cs
@@ -38,7 +38,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Location = new System.Drawing.Point(-5, -5);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1083, 590);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
